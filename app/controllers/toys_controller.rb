caabdacfd8eda9b1aa96c249b0dabcb3945194db
Zachary Sain <zachsain@gmail.com>
2022-08-10 13:08:07 -0500

    completed

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -7,18 +7,20 @@ class ToysController < ApplicationController
   end
 
   def create
-    toy = Toys.create(toy_params)
+    toy = Toy.create(toy_params)
     render json: toy, status: :created
   end
 
   def update
     toy = Toy.find_by(id: params[:id])
     toy.update(toy_params)
+    render json: toy
   end
 
   def destroy
     toy = Toy.find_by(id: params[:id])
     toy.destroy
+    byebug
     head :no_content
   end
 
